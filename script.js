021df628990a5385be07e2faabaf9040b534a89b
princekumarpaswan <prasantandal123@gmail.com>
2023-02-09 22:58:50 +0530

    added animation

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-import anime from './anime-master/lib/anime.es'
+console.log('working');
+
+
+
+const animation = () => {
+    const timeline = anime.timeline({
+        duration: 1000,
+        easing: "easeOutExpo"
+    });
+    timeline.add({
+        targets: '.targetleft',
+        translateX: 250,
+        uration: 3000,
+
+    });
+    timeline.add({
+        targets: '.targetRight',
+        translateX: -250,
+        direction: 'reverse',
+        uration: 3000,
+    }, '-=1000')
+    document.getElementById('left').style.display = 'none'
+    document.getElementById('right').style.display = 'none'
+
+}
+
+
+
+const handleClick = () => {
+    // return alert("done")
+    animation()
+}
+
