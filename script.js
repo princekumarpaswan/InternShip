a04f49a6f48c8f062c15b3d1543cfe9185a70165
princekumarpaswan <prasantandal123@gmail.com>
2023-02-10 23:58:53 +0530

    added new file

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ console.log('working');
 
 
 const animation = () => {
+
+    // animation starts
+
     const timeline = anime.timeline({
-        duration: 1000,
-        easing: "easeOutExpo"
+        duration: 900,
+        easing: 'easeOutExpo'
     });
     timeline.add({
         targets: '.targetleft',
@@ -18,16 +21,91 @@ const animation = () => {
         translateX: -250,
         direction: 'reverse',
         uration: 3000,
+    }, '-=900')
+    const hide = () => {
+
+        // dom manuplation starts here
+
+        const topTitle = document.getElementById('pick')
+        topTitle.classList.add("visually-hidden")
+
+
+        const Title = document.getElementById('pickMost')
+        Title.classList.add("visually-hidden")
+
+
+        const downList = document.getElementById('edit')
+        downList.classList.add("visually-hidden")
+
+
+        document.getElementById('left').style.display = 'none'
+        document.getElementById('right').style.display = 'none'
+
+
+        const centerNonV = document.getElementById('centerNV')
+        centerNonV.classList.add("visually-hidden")
+
+        const pNV = document.getElementById('paNV')
+        pNV.classList.add("visually-hidden")
+
+        document.getElementById('hideS').style.display = "block"
+
+
+        const sidecard = () => {
+            document.getElementById('hideS').style.display = "none"
+            const sideData = document.getElementById('centerV')
+            sideData.classList.remove('visually-hidden')
+
+
+            const spanText = document.getElementById('spanText')
+            spanText.classList.add('visually-hidden')
+            spanText.style.transition = "all 2s"
+
+
+            const btnText = document.getElementById('btnText')
+            btnText.classList.remove('visually-hidden')
+
+            const backG = document.getElementById('backG')
+            backG.style.transition = "all 2s ease "
+            backG.style.backgroundImage = "url('./back.png')"
+
+
+
+            const load = document.getElementById('kmap')
+            load.classList.add('visually-hidden')
+            const dateData = document.getElementById('dateData')
+            dateData.classList.remove('visually-hidden')
+
+
+
+            const reset = document.getElementById('reset')
+            reset.innerText = 'Choose Base Plan'
+
+
+
+        }
+        setTimeout(sidecard, 1000)
+
+
+    }
+    setTimeout(hide, 400)
+
+    timeline.add({
+        targets: '.targetCenter',
+        translateX: 320,
+        uration: 3000,
+    })
+    timeline.add({
+        targets: '.targetButton',
+        translateX: 450,
+        uration: 3000,
     }, '-=1000')
-    document.getElementById('left').style.display = 'none'
-    document.getElementById('right').style.display = 'none'
 
 }
 
 
 
 const handleClick = () => {
-    // return alert("done")
     animation()
 }
 
